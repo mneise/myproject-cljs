fc90aede126271f7ec7dd0238df64f362d551f4c
Maria Neise <mneise@liveops.com>
2016-05-10 16:42:27 +1200

    Update lein-cljsbuild version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]]
-  :plugins [[lein-cljsbuild "1.1.3"]]
+  :plugins [[lein-cljsbuild "1.1.4-SNAPSHOT"]]
   :source-paths ["src/cljs"]
   :java-source-paths ["src/java"]
   :cljsbuild {:builds {:dev {:source-paths ["src/cljs"]
